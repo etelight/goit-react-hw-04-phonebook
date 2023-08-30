4f00ccd6aca9c75f9053e9dc77819fbbbad49ef0
etelight <etelight@gmail.com>
2023-08-30 17:26:09 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,7 @@ const initialContacts = [
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
+
 const getContacts = () => {
   const savedContacts = localStorage.getItem('contacts');
   if (savedContacts !== null) {
